648566b9a800511a98d197cab02b6d854483994c
tushar1854 <86524985+tushar1854@users.noreply.github.com>
2024-01-04 16:56:59 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -10,12 +10,6 @@ from content import content_api_result
 from collaborative import collaborative_api_result
 from history import history_result
 
-# Your Spotify API credentials
-CLIENT_ID = "c8500a3536794491ad7987729b8b5dc0"
-CLIENT_SECRET = "59cf177d543c49929c094ec1393c047e"
-ACCESS_TOKEN_URL = "https://accounts.spotify.com/api/token"
-BASE_URL = "https://api.spotify.com/v1/search"
-
 
 app = Flask(__name__)
 
@@ -37,17 +31,6 @@ def login():
 @app.route("/api/top50")
 def top50():
     lst = get_top_50_song()
-    # access_token = get_access_token(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN_URL)
-    # for i in range(len(lst)):
-    #     # Exception
-    #     if lst[i]["artist"] == "Florence + The Machine":
-    #         artist = "The Machine"
-    #     else:
-    #         artist = lst[i]["artist"]
-    #     img = get_images_lst(lst[i]["trackname"],
-    #                          artist, BASE_URL, access_token)
-    #     lst[i]["img"] = img
-
     return jsonify(lst)
 
 
@@ -56,13 +39,6 @@ def content_based():
     songname = request.args.get('songname').lower()
     artist = request.args.get('artist').lower()
     lst = content_api_result(songname, artist)
-    # For fetching img
-    # access_token = get_access_token(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN_URL)
-    # for i in range(len(lst)):
-    #     img = get_images_lst(lst[i]['trackname'],
-    #                          lst[i]['artist'], BASE_URL, access_token)
-    #     lst[i]["img"] = img
-
     return lst
 
 
@@ -89,7 +65,6 @@ def history():
     return jsonify(result)
 
 
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0")
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
